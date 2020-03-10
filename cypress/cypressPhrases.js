0342595da488aed483339b36ce3033e5bcd33f2c
Dakad <3106338+Dakad@users.noreply.github.com>
2020-03-10 19:48:41 +0100

    Clean cypress regex phrases

--- cypress/cypressPhrases.js
+++ cypress/cypressPhrases.js
@@ -1,98 +1,94 @@
-const {
-    scroll,
-    click,
-    type,
-    replace,
-    open,
-    waitForResults,
-    dragAbove,
-    takeSnapshot,
-    takeElSnapshot,
-    takeNamedSnapshot,
-    onPage,
-    redirectedTo,
-    nElements,
-    textOnEl,
-    elExists,
-    elHasValue,
-    elDoesNotExist,
-    elBackground,
-    elBorder,
-} = require('./cypressFunctions');
+const cypressFunctions = require('./cypressFunctions');
 
 const REGEX = require('../common/regex');
 
 module.exports = () => {
     // ex: I scroll to the bottom the "Modal"
-    When(REGEX.SCROLL, scroll);
+    When(REGEX.SCROLL, cypressFunctions.scroll);
 
     // ex:  I click on the "Button"
     //      I click "Save"
     //      I click on "Save" inside the "Modal"
     //      I click on "Button" inside the "Modal" containing "Save"
-    When(REGEX.CLICK, click);
+    When(REGEX.CLICK, cypressFunctions.click);
 
 
     // ex:  I type "toli" into the "Username Input"
     //      I type "toli" into "Username"
     //      I type "toli" into the "Username Input" on the "SignIn form"
-    When(REGEX.TYPE, type);
+    When(REGEX.TYPE, cypressFunctions.type);
+
 
     // ex:  When I replace the contents of "Username" with "toli"
     //      When I replace the contents of "Username" inside of the "Login Modal" with "toli"
-    When(REGEX.REPLACE, replace);
+    When(REGEX.REPLACE, cypressFunctions.replace);
+
 
     // ex: I open the "Login Screen"
-    When(REGEX.OPEN, open);
+    When(REGEX.OPEN, cypressFunctions.open);
+
 
     // @TODO: Figure out while default way isn't working
-    When(REGEX.WAIT_FOR_RESULTS, waitForResults);
+    When(REGEX.WAIT_FOR_RESULTS, cypressFunctions.waitForResults);
+
 
     // use only in cases where Cypress functions can't be used
-    When(REGEX.WAIT_SECONDS, (seconds) => {
-        cy.wait(seconds * 1000);
-    });
+    When(REGEX.WAIT_SECONDS, cypressFunctions.wait);
+
 
     // This is experimental and not part of the official API
-    When(REGEX.DRAG_ABOVE, dragAbove);
+    When(REGEX.DRAG_ABOVE, cypressFunctions.dragAbove);
 
-    When(REGEX.TAKE_SNAPSHOT, takeSnapshot);
 
-    When(REGEX.TAKE_EL_SNAPSHOT, takeElSnapshot);
+    When(REGEX.TAKE_SNAPSHOT, cypressFunctions.takeSnapshot);
 
-    When(REGEX.TAKE_SNAPSHOT_NAMED, takeNamedSnapshot)
 
+    When(REGEX.TAKE_EL_SNAPSHOT, cypressFunctions.takeElSnapshot);
+
+
+    When(REGEX.TAKE_SNAPSHOT_NAMED, cypressFunctions.takeNamedSnapshot)
+
+    
     // ex: I should be on the "Login Screen"
-    Then(REGEX.ON_PAGE, onPage);
+    Then(REGEX.ON_PAGE, cypressFunctions.onPage);
+
 
     // ex: I should be redirected to the "Login Screen"
-    Then(REGEX.REDIRECTED_TO, redirectedTo);
+    Then(REGEX.REDIRECTED_TO, cypressFunctions.redirectedTo);
+
 
     // I should see 3 "Buttons" in "Modal"
-    Then(REGEX.N_ELEMENTS, nElements);
+    Then(REGEX.N_ELEMENTS, cypressFunctions.nElements);
+
 
     // ex: I should see "Press Me" on the "Button" inside the "Modal"
-    Then(REGEX.TEXT_ON_EL, textOnEl);
+    Then(REGEX.TEXT_ON_EL, cypressFunctions.textOnEl);
+
 
     // putting after because the one before exclusively works
     // for text and doesn't have a verb before it
     // ex: I should see "Press Me Button" on the "Button" inside the "Modal"
-    Then(REGEX.EL_EXISTS, elExists);
+    Then(REGEX.EL_EXISTS, cypressFunctions.elExists);
+
 
     // ex:  I should not see the "Buttons" in the "Modal"
     //      I should not see "Buttons" on the "Page"
     //      I should not see the "Button"
-    Then(REGEX.EL_DOES_NOT_EXIST, elDoesNotExist);
+    Then(REGEX.EL_DOES_NOT_EXIST, cypressFunctions.elDoesNotExist);
+
 
     // ex: "Username" should be "toli"
-    Then(REGEX.EL_CONTAINS_TEXT, elHasValue);
+    Then(REGEX.EL_CONTAINS_TEXT, cypressFunctions.elHasValue);
+
 
     // ex: "Username's" value should be "toli"
-    Then(REGEX.EL_VALUE, elHasValue);
+    Then(REGEX.EL_VALUE, cypressFunctions.elHasValue);
+
 
     // ex: I should see a "red" background on the "Button"
-    Then(REGEX.EL_BACKBGROUND, elBackground)
+    Then(REGEX.EL_BACKBGROUND, cypressFunctions.elBackground)
+
 
     // ex: I should see a "red" border on the "Button"
-    Then(REGEX.EL_BORDER, elBorder)
+    Then(REGEX.EL_BORDER, cypressFunctions.elBorder)
 }
