4222fe072d500c55998c839826c2a6404413465d
Dakad <3106338+Dakad@users.noreply.github.com>
2020-03-17 12:51:30 +0100

    Replace Screens by Pages

--- common/variables.js
+++ common/variables.js
@@ -1,8 +1,8 @@
 // will hold element selectors throughout the test
 let ELEMENT_SELECTORS = {};
 
-// will hold screen urls throughout the test
-let SCREENS = {};
+// will hold page urls throughout the test
+let PAGES = {};
 
 // will hold variables used throughout the test suite 
 let STATE = {};
@@ -11,8 +11,8 @@ const setElementSelector = selectors => {
     Object.assign(ELEMENT_SELECTORS, selectors);
 };
 
-const setScreens = screens => {
-    Object.assign(SCREENS, screens);
+const setPages = pages => {
+    Object.assign(PAGES, pages);
 }
 
 const setState = (variable, value) => {
@@ -21,9 +21,9 @@ const setState = (variable, value) => {
 
 module.exports = {
     ELEMENT_SELECTORS,
-    SCREENS,
+    PAGES,
     STATE,
     setElementSelector,
-    setScreens,
+    setPages,
     setState,
 };
